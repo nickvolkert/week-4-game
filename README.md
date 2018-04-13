2c49dd21d2950f3324c3977e5f19740368117af9
Nick Volkert <nick@nickvolkert.com>
2018-04-13 00:17:06 -0500

    finishing touches

---
+++ README.md
@@ -0,0 +1,20 @@
+# Not all that is glitters is gold
+
+### Overview
+
+Gems! Each gem has a randomly assigned value (set my javascript), and by clicking, tinkering, adding and clicking some more, the goal is to match the 'Cystal score to match' to win the game! Simple and fun. Click game instructions to learn more, or just start clicking!
+
+### Before You Begin
+
+This is the week 4 assignment as a part of my pursuit for the Northwestern FullStack Bootcamp Certification.
+
+The project uses the following technology:
+1. HTML, CSS and jQuery
+
+2. SVG sprites (that I illustrated)
+
+3. Google Fonts
+
+4. And favicons that change state based on your wins and losses.
+
+Like what you see? Work with me by contacting me at nick@nickvolkert.com, or view more of my work at nickvolkert.com
--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,116 +1,128 @@
-var userWins = 0;
-var userLosses = 0;
-var userScore = 0;
-var redGemNum;
-var diamondNum;
-var yelGemNum;
-var squareGemNum;
-var matchScore;
-//Generates the random Gem Numbers
-function randomGemGenerator(){
-  redGemNum = parseInt(Math.floor(Math.random() * 12) + 1);
-  diamondNum = parseInt(Math.floor(Math.random() * 12) + 1);
-  yelGemNum = parseInt(Math.floor(Math.random() * 12) + 1);
-  squareGemNum = parseInt(Math.floor(Math.random() * 12) + 1);
-  matchScore = Math.floor(Math.random()*(120-19+1)+19);
-  console.log("Match Score: " + matchScore);
-  console.log("User Score: " + userScore);
-}
-//Adds the Gem clicks to the User's Score
-function scoreBucket(){
-  $("#totalScore").text(userScore);
-}
-//resets the numbers i think - i hope
-function clearNumbers(){
-  redGemNum = 0;
-  diamondNum = 0;
-  yelGemNum = 0;
-  squareGemNum = 0;
-  console.log("Clear Numbers is working");
-}
-//Checks wins and losses
-function winLossCheck(){
-  if (userScore == matchScore){
-    //User Wins!
-    userWins++;
-    $("#winTracker").text(userWins++);
-    $("#intCurtain").show();
-    $("#winScreen").show();
-    $("#lossScreen").hide();
-    $("#gameInstructions").hide();
-    console.log("is win working?");
-  } else if (userScore > matchScore){
-    //User Lost
-    userLosses++;
-  	$("#lossTracker").text(userLosses);
-    $("#intCurtain").show();
-    $("#lossScreen").show();
-    $("#winScreen").hide();
-    $("#gameInstructions").hide();
-    console.log("is loss working?");
-  }
-}
+jQuery(function(){
+	weekFourGame.init();
+});
 
-//Game Instructions Modal
-function gameInst(){
-  $("#howToLink a").on("click", function(){
-    $("#intCurtain").show();
-    $("#gameInstructions").show();
-  });
-  $("#btnInstructions").on("click", function(){
-    $("#intCurtain").hide();
-    $("#gameInstructions").hide();
-    $("#lossScreen").hide();
-    $("#winScreen").hide();
-  });
-}
+var weekFourGame = (function ($){
+	function init () {
+      var userWins = 0;
+      var userLosses = 0;
+      var userScore = 0;
+      var redGemNum;
+      var diamondNum;
+      var yelGemNum;
+      var squareGemNum;
+      var matchScore;
+      //Generates the random Gem Numbers
+      function randomGemGenerator(){
+        redGemNum = parseInt(Math.floor(Math.random() * 12) + 1);
+        diamondNum = parseInt(Math.floor(Math.random() * 12) + 1);
+        yelGemNum = parseInt(Math.floor(Math.random() * 12) + 1);
+        squareGemNum = parseInt(Math.floor(Math.random() * 12) + 1);
+        matchScore = Math.floor(Math.random()*(120-19+1)+19);
+        console.log("Match Score: " + matchScore);
+        console.log("User Score: " + userScore);
+      }
+      //Adds the Gem clicks to the User's Score
+      function scoreBucket(){
+        $("#totalScore").text(userScore);
+      }
+      //resets the numbers i think - i hope
+      function clearNumbers(){
+        redGemNum = 0;
+        diamondNum = 0;
+        yelGemNum = 0;
+        squareGemNum = 0;
+        console.log("Clear Numbers is working");
+      }
+      //Checks wins and losses
+      function winLossCheck(){
+        if (userScore == matchScore){
+          //User Wins!
+          userWins++;
+          $("#favicon").attr("href","assets/img/win.jpg");
+          $("#winTracker").text(userWins++);
+          $("#intCurtain").show();
+          $("#winScreen").show();
+          $("#lossScreen").hide();
+          $("#gameInstructions").hide();
+          console.log("is win working?");
+        } else if (userScore > matchScore){
+          //User Lost
+          userLosses++;
+          $("#favicon").attr("href","assets/img/lose.jpg");
+        	$("#lossTracker").text(userLosses);
+          $("#intCurtain").show();
+          $("#lossScreen").show();
+          $("#winScreen").hide();
+          $("#gameInstructions").hide();
+          console.log("is loss working?");
+        }
+      }
 
-//Close all modals
-function closeCurtain(){
-  $(".playAgain").on("click", function(){
-    $("#gameInstructions").hide();
-    $("#intCurtain").hide();
-    $("#lossScreen").hide();
-    $("#winScreen").hide();
-    gamePlay();
-  });
-}
-//Game Click Fuction
-function gamePlay(){
-    userScore = 0;
-    clearNumbers();
-    randomGemGenerator();
-    scoreBucket();
-    $("#currentScore h2").text(matchScore);
-    //Red Gems
-}
-$("#redGem").on("click", function(){
-  userScore += redGemNum;
-  scoreBucket();
-  console.log("Red Gem = " + redGemNum);
-  winLossCheck();
-});
-//Diamonds
-$("#diamond").on("click", function(){
-  userScore += diamondNum;
-  scoreBucket();
-  console.log("Diamond = " + diamondNum);
-  winLossCheck();
-});
-//Yellow Gems
-$("#yellowGem").on("click", function(){
-  userScore += yelGemNum;
-  scoreBucket();
-  console.log("Yellow Gem = " + yelGemNum);
-  winLossCheck();
-});
-//Square Gems
-$("#squareGem").on("click", function(){
-  userScore += squareGemNum;
-  scoreBucket();
-  console.log("Square Gem = " + squareGemNum);
-  winLossCheck();
-});
-gamePlay();
-gameInst();
-closeCurtain();
+      //Game Instructions Modal
+      function gameInst(){
+        $("#howToLink a").on("click", function(){
+          $("#intCurtain").show();
+          $("#gameInstructions").show();
+        });
+        $("#btnInstructions").on("click", function(){
+          $("#intCurtain").hide();
+          $("#gameInstructions").hide();
+          $("#lossScreen").hide();
+          $("#winScreen").hide();
+        });
+      }
+
+      //Close all modals
+      function closeCurtain(){
+        $(".playAgain").on("click", function(){
+          $("#gameInstructions").hide();
+          $("#intCurtain").hide();
+          $("#lossScreen").hide();
+          $("#winScreen").hide();
+          gamePlay();
+        });
+      }
+      //Game Click Fuction
+      function gamePlay(){
+          userScore = 0;
+          clearNumbers();
+          randomGemGenerator();
+          scoreBucket();
+          $("#currentScore h2").text(matchScore);
+          //Red Gems
+      }
+      $("#redGem").on("click", function(){
+        userScore += redGemNum;
+        scoreBucket();
+        console.log("Red Gem = " + redGemNum);
+        winLossCheck();
+      });
+      //Diamonds
+      $("#diamond").on("click", function(){
+        userScore += diamondNum;
+        scoreBucket();
+        console.log("Diamond = " + diamondNum);
+        winLossCheck();
+      });
+      //Yellow Gems
+      $("#yellowGem").on("click", function(){
+        userScore += yelGemNum;
+        scoreBucket();
+        console.log("Yellow Gem = " + yelGemNum);
+        winLossCheck();
+      });
+      //Square Gems
+      $("#squareGem").on("click", function(){
+        userScore += squareGemNum;
+        scoreBucket();
+        console.log("Square Gem = " + squareGemNum);
+        winLossCheck();
+      });
+      gamePlay();
+      gameInst();
+      closeCurtain();
+} return {
+       init:init
+   };
+})(jQuery);
